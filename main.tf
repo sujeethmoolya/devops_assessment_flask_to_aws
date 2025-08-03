a54324879931433b98aa4e37e7a65897c5997d6f
Sujeeth Moolya <smartsujeeth19@gmai..com>
2025-08-04 00:52:55 +0530

    setting up security group

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ module "networking" {
   eu_availability_zone = var.eu_availability_zone
   cidr_private_subnet  = var.cidr_private_subnet
 }
+
+module "security_group" {
+  source              = "./security-groups"
+  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+  vpc_id              = module.networking.dev_proj_1_vpc_id
+  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+}
