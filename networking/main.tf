b7e39823b8838f9e58201fa8716c8fb909f3525f
Sujeeth Moolya <smartsujeeth19@gmai..com>
2025-08-03 23:51:44 +0530

    Setting VPC, Private and Public Subnet for Jenkins

---
+++ networking/main.tf
@@ -0,0 +1,50 @@
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "cidr_public_subnet" {}
+variable "eu_availability_zone" {}
+variable "cidr_private_subnet" {}
+
+output "dev_proj_1_vpc_id" {
+  value = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+}
+
+output "dev_proj_1_public_subnets" {
+  value = aws_subnet.dev_proj_1_public_subnets.*.id
+}
+
+output "public_subnet_cidr_block" {
+  value = aws_subnet.dev_proj_1_public_subnets.*.cidr_block
+}
+
+# Setup VPC
+resource "aws_vpc" "dev_proj_1_vpc_eu_central_1" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+
+# Setup public subnet
+resource "aws_subnet" "dev_proj_1_public_subnets" {
+  count             = length(var.cidr_public_subnet)
+  vpc_id            = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  cidr_block        = element(var.cidr_public_subnet, count.index)
+  availability_zone = element(var.eu_availability_zone, count.index)
+
+  tags = {
+    Name = "dev-proj-public-subnet-${count.index + 1}"
+  }
+}
+
+# Setup private subnet
+resource "aws_subnet" "dev_proj_1_private_subnets" {
+  count             = length(var.cidr_private_subnet)
+  vpc_id            = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  cidr_block        = element(var.cidr_private_subnet, count.index)
+  availability_zone = element(var.eu_availability_zone, count.index)
+
+  tags = {
+    Name = "dev-proj-private-subnet-${count.index + 1}"
+  }
+}
