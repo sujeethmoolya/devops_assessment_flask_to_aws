3e3f2c3b8abe243f82081df04c511b707e4d65ec
Sujeeth Moolya <smartsujeeth19@gmai..com>
2025-08-04 03:47:57 +0530

    Creating Target group and adding our ec2 instance in it

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "networking" {
   source               = "./networking"
-  source               = "./networking" 
   vpc_cidr             = var.vpc_cidr
   vpc_name             = var.vpc_name
   cidr_public_subnet   = var.cidr_public_subnet
@@ -25,4 +24,13 @@ module "jenkins" {
   sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_jenkins_port_8080]
   enable_public_ip_address  = true
   user_data_install_jenkins = templatefile("./jenkins-runner-script/jenkins-installer.sh", {})
+}
+
+module "lb_target_group" {
+  source                   = "./load-balancer-target-group"
+  lb_target_group_name     = "jenkins-lb-target-group"
+  lb_target_group_port     = 8080
+  lb_target_group_protocol = "HTTP"
+  vpc_id                   = module.networking.dev_proj_1_vpc_id
+  ec2_instance_id          = module.jenkins.jenkins_ec2_instance_ip
 }
