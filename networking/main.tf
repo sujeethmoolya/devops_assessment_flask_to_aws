221254c6952ac70fc75802715ea546db67404a90
Sujeeth Moolya <smartsujeeth19@gmai..com>
2025-08-04 00:43:36 +0530

    setting up subnet association

--- networking/main.tf
+++ networking/main.tf
@@ -69,6 +69,13 @@ resource "aws_route_table" "dev_proj_1_public_route_table" {
   }
 }
 
+# Public Route Table and Public Subnet Association
+resource "aws_route_table_association" "dev_proj_1_public_rt_subnet_association" {
+  count          = length(aws_subnet.dev_proj_1_public_subnets)
+  subnet_id      = aws_subnet.dev_proj_1_public_subnets[count.index].id
+  route_table_id = aws_route_table.dev_proj_1_public_route_table.id
+}
+
 # Private Route Table
 resource "aws_route_table" "dev_proj_1_private_subnets" {
   vpc_id = aws_vpc.dev_proj_1_vpc_eu_central_1.id
@@ -76,4 +83,11 @@ resource "aws_route_table" "dev_proj_1_private_subnets" {
   tags = {
     Name = "dev-proj-1-private-rt"
   }
+}
+
+# Private Route Table and private Subnet Association
+resource "aws_route_table_association" "dev_proj_1_private_rt_subnet_association" {
+  count          = length(aws_subnet.dev_proj_1_private_subnets)
+  subnet_id      = aws_subnet.dev_proj_1_private_subnets[count.index].id
+  route_table_id = aws_route_table.dev_proj_1_private_subnets.id
 }
