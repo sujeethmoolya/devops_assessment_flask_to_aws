272a4a7bb28425f6700468160e5a3d5d5f78f33e
Sujeeth Moolya <smartsujeeth19@gmai..com>
2025-08-04 02:57:09 +0530

    Key File updated

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 module "networking" {
-  source               = "./networking"
   source               = "./networking" 
   vpc_cidr             = var.vpc_cidr
   vpc_name             = var.vpc_name
